1eac4c97c5c94e4b8fdd12049854a5eb126d9c96
Jemma Marie <JemmaMarie@iMacs-iMac-9.home>
2016-08-10 00:20:29 -0700

    commit final changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,11 @@
 // Initial Topics - Pregenerated
-
-var topics = ['Disneyland', 'Pixar', 'HIMYM', 'Huskies', 'Puppies', 'Super Smash Brothers', 'Pandas', 'Chimpanzees', 'Johnny Depp', 'Harry Potter' ];
-
-// For loop that will go through the Topics Array and turn them into buttons
-
-function renderButtons(){
-	$('#viewGiphy').empty(); //prevents the div to keep appending the same set of buttons
-	
-	for (var i = 0; i < topics.length; i++){
-		var gifButtons = $("<button>")
-
-		//we can now call on all the buttons using its class
-		gifButtons.addClass("myFavThings");
-
-		//set name attributes for the buttons based on its name
-
-		gifButtons.attr("data-name", topics[i]);
-		gifButtons.text(topics[i]);
-
-		// append the new buttons into the viewGiphy div
-		$("#buttonDiv").append(gifButtons);
-	}
-
-}
-
-// Function that will take in new topics and turn them into buttons as well
-$('#addTopic').on('click', function(){
-	$("viewGiphy").empty();
-
-	var newTopic = $('#topic_input').val(); //will take the value that is entered in the form box
-	topics.push(newTopic); //will push the inputs into the topics array with the pre-made buttons
-
-	renderButtons(); //calls the make button function
-	return false;
-});
+$(function(){
+var topics = ['Disneyland', 'Disney World of Color', 'Pixar', 'Harry Potter', 'HIMYM', 'The Walking Dead', 'The Flash', 'The Arrow', 'Grant Gustin', 'Huskies', 'Bears', 'Pandas', 'Super Smash Brothers' ];
 
 function displayGiphy(){
+
 	var topicName = $(this).data("name");
-	var gifNumber = "&limit=20";
+	var gifNumber = "&limit=12";
 
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+topicName+gifNumber+"&api_key=dc6zaTOxFJmzC";
  	
@@ -54,9 +22,16 @@ function displayGiphy(){
 	// setting data-state attributes to images/gifs
 			var gif_image = $('<img>')
 			.attr('src', getData[i].images.fixed_height_still.url)
-			.attr('data-still', getData[i].images.fixed_height_still.url)
-			.attr('data-animate', getData[i].images.fixed_height.url)
-			.attr('data-state', "still");
+			.click(function(){
+				var originalSource = $(this).attr("src");
+				if(originalSource.indexOf("_s.gif") !== -1){
+					var newSource = originalSource.replace("_s.gif", ".gif");
+				}else{
+					var newSource = originalSource.replace(".gif","_s.gif");
+				}
+			$(this).attr("src", newSource); 
+
+			});
 			
 	// appends the rating and the giff into the new div that i made
 			giphyDiv.append(ratingText);
@@ -67,21 +42,44 @@ function displayGiphy(){
 		}
 	});	
 }
-$( document ).on( "click", ".myFavThings", function () {
-        var state = $( this ).attr( "data-state" );
 
-        if ( state == "still" ) {
-            $( this ).attr( "src", $( this ).data( "animate" ) );
-            $( this ).attr( "data-state", $( this ).data( "animate" ) );
-        } else {
-            $( this ).attr( "src", $( this ).data( "still" ) );
-            $( this ).attr( "data-state", "still" );
-        }
-        return false;
-    });
+
+// For loop that will go through the Topics Array and turn them into buttons
+function renderButtons(){
+
+	$('#buttonDiv').empty(); //prevents the div to keep appending the same set of buttons
+
+	for (var i = 0; i < topics.length; i++){
+
+		var gifButtons = $("<button>")
+
+		//we can now call on all the buttons using its class
+		gifButtons.addClass("myFavThings");
+
+		//set name attributes for the buttons based on its name
+
+		gifButtons.attr("data-name", topics[i]);
+		gifButtons.text(topics[i]);
+
+		// append the new buttons into the viewGiphy div
+		$("#buttonDiv").append(gifButtons);
+
+	}
+
+}
+// Function that will take in new topics and turn them into buttons as well
+$('#addTopic').on('click', function(){
+	var newTopic = $('#topic_input').val().trim(); //will take the value that is entered in the form box
+	topics.push(newTopic); //will push the inputs into the topics array with the pre-made buttons
+	
+	renderButtons(); //calls the make button function
+	return false;
+})
+
 
 //the browser/window will initiate the displayGiphy function when anything in the viewGiphy div is clicked
 $(document).on('click', '.myFavThings', displayGiphy);
 
 
 renderButtons();
+})
